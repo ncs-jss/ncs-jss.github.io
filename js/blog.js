af7482dc92abc336d9799e62c2de218724cebef2
Anurag Srivastava <anurag.as.srivastava@gmail.com>
2020-05-28 14:08:17 +0530

    Fixed font and links

--- js/blog.js
+++ js/blog.js
@@ -1,35 +1,27 @@
-//Async request to JSON file for Blog post
-const getPosts = async () => {
-  const response = await fetch("../js/Blog.json");
-  const data = response.json();
-  return data;
-};
-
 //getting section for showing blog data
 const section = document.querySelector("#blog-text");
 let html = ``;
-
 //template
 const addTemplate = (post) => {
-  let date = moment(post.published_on).format("LL");
-
   let { body } = post;
   let description = body.length > 50 ? `${body.substring(0, 150)}...` : body; //body truncate
   html = `
+  <a style="text-decoration: none; color: #000000" class="aveniDemi" href=${post.link} target="_blank">
   <div
   class="col-md-4 pt-1 text-center"
   style="padding-left: 0; padding-right: 0;"
 >
+
   <div
     class="club_card justify-content-center mx-auto"
     style="margin-bottom: 15px; padding-bottom: 10px;"
   >
-    <img
-      class="card-img-top"
-      style="width: 90%; height: 170px;"
-      src="${post.image}"
+    <div
+      class="card-img-top img-responsive mx-auto"
+      style='width: 90%; height: 170px; 
+      background: url("${post.image}"); background-size: cover; border-radius: 10px;'
       alt="Card image cap"
-    />
+    /></div>
     <div
       class="card-body"
       style="padding-bottom: 0px;"
@@ -37,7 +29,7 @@ const addTemplate = (post) => {
     >
       <h4 class="avenirDemi">${post.title}</h4>
       <span class="author text-muted">by ${post.author}</span>
-      <p class="card-text pt-3 text-muted">
+      <p class="avenirRegular w-75 pt-3 text-muted">
         ${description}
       </p>
     </div>
@@ -50,21 +42,20 @@ const addTemplate = (post) => {
         width: 90%;
       "
     >
-      <div class="date">${date}</div>
+      <div class="date">${post.published_on}</div>
       <div class="source"><a href="${post.link}" target="_blank"><img style="height: 30px; width: 30px" src="../img/icons/blog/${post.source}.png"</img></a></div>
     </div>
-  </div>
+  </>
+  
 </div>
+</a>
 `;
+
   section.innerHTML += html;
 };
-
-//calling aysnc function on page load
-window.addEventListener("load", () => {
-  getPosts().then((data) => {
-    const arr = data.posts.slice(-3); //for last three posts
-    arr.forEach((post) => {
-      addTemplate(post);
-    });
+$.getJSON("../js/Blog.json", (json) => {
+  const arr = json.slice(-3);
+  arr.forEach((obj) => {
+    addTemplate(obj);
   });
 });
