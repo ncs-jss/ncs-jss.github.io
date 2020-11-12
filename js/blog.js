98ab5c4d7ffda61c07b7d1b4e617a8c6a2866540
SingingApple <anurag.as.srivastava@gmail.com>
2020-11-13 01:07:47 +0530

    Fonts and Med Logo

--- js/blog.js
+++ js/blog.js
@@ -11,21 +11,21 @@ const addTemplate = (post) => {
   html = `
   <a style="text-decoration: none; color: #000000" class="aveniDemi" href=${post.link} target="_blank">
     <div class="col-md-3 pt-1 text-center" style="padding-left: 0; padding-right: 0;">
-      <div class="club_card justify-content-center mx-auto" id="blog" style="margin-bottom: 15px; padding-bottom: 10px; border-radius: 10px;">
-        <div class="card-img-top img-responsive mx-auto" style='width: 90%; height: 130px;  background: url("${post.image}"); background-size: cover; border-radius: 10px;' alt="Card image cap"/>
+      <div class="club_card justify-content-center mx-auto" style="margin-bottom: 15px; padding-bottom: 10px;">
+        <div class="card-img-top img-responsive mx-auto" style='width: 90%; height: 140px;  background: url("${post.image}"); background-size: cover; border-radius: 10px;' alt="Card image cap"/>
       </div>
       <div class="card-body" style="padding-bottom: 0px;" align="left">
-        <h4 class="avenirDemi" style="font-size: 1.2rem;">${postTitle}</h4>
+        <h4 class="avenirDemi" style="font-weight: 500; font-size: 24px; line-height: 33px;">${postTitle}</h4>
         <div class="source">
-          <a href="${post.link}" target="_blank"><img style="height: 30px; width: 30px" src="../img/icons/blog/${post.source}.png"</img></a></div>
+          <a href="${post.link}" target="_blank"><img style="height: 20px; width: 20px" src="../img/icons/blog/${post.source}.png"</img></a></div>
         </div>
-        <hr style="width: 90%;"/>
+        <hr style="width: 90%; margin-bottom:15px;"/>
         <a style="text-decoration:none; color:#000000" href=${post.link}>
         <div class="footer d-flex mx-auto" style="padding-bottom: 10px; width: 90%; align-items: center; cursor: pointer;">
-          <img src=${post.authorImage} style="height: 40px; width: 40px;" class="rounded-circle" />
-          <div class="author-info d-flex" style="flex-direction: column; align-items: flex-start; margin-left: 14px;">  
-            <span class="author-name avenirDemi">${postAuthor}</span>
-            <span class="date text-muted">${post.published_on}</span>
+          <img src=${post.authorImage} style="height: 36px; width: 36px;" class="rounded-circle" />
+          <div class="author-info d-flex" style="flex-direction: column; align-items: flex-start; margin-left: 15px;">  
+            <span class="avenirDemi author-name" style="font-size: 16px; line-height: 19px; color: rgba(0,0,0,0.8)">${postAuthor}</span>
+            <span class="date text-muted" style="font-family:'AvenirNext LT Pro';font-size: 12px; line-height: 24px; ">${post.published_on}</span>
           </div>
         </div>
         </a>
