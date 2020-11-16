21be6b5014a0450a61b224425be5f83d78d3f1c0
SingingApple <anurag.as.srivastava@gmail.com>
2020-11-16 21:48:08 +0530

    blog-content

--- js/blog.js
+++ js/blog.js
@@ -24,19 +24,19 @@ const addTemplate = (post) => {
       <div class="club_card blog_card justify-content-center mx-auto" style="margin-bottom: 15px; padding-bottom: 10px;">
         <div class="card-img img-responsive mx-auto" style='width: 90%; min-height: 140px;  background: url("${post.image}"); background-size: cover;' alt="Card image cap"/>
       </div>
-      <div class="card-body" style="padding-bottom: 0px;" align="left">
-        <h4 title="${title}" class="avenirDemi" style="font-weight: 500; font-size: 24px; line-height: 33px;">${postTitle}</h4>
+      <div class="card-body" style="padding-bottom: 0px; padding-left: inherit;" align="left">
+        <h4 title="${title}" class="avenirDemi" style="font-weight: 500; font-size: 24px; line-height: 33px;">${title}</h4>
         <a style="color: #000000" href="${post.link}" target="_blank">
-        <div class="source">
-          <img style="height: 20px; width: 20px" src="../img/icons/blog/${post.source}.png"</img></div>
+        <div class="source" style="margin-top: 7px;">
+          <img style="height: 20px; width: 20px" src="../img/icons/blog/${post.source}.svg"</img></div>
         </div>
         </a>
-        <a style="text-decoration:none; color:#000000 href=${post.link} target="_blank"><hr style="width: 90%; margin-bottom:15px;"/>
+        <a style="text-decoration:none; color:#000000 href=${post.link} target="_blank"><hr style="width: 90%; margin-bottom:15px; margin-top:25px;"/>
         <a style="text-decoration:none; color:#000000" href=${post.link} target="_blank">
         <div class="footer d-flex mx-auto" style="padding-bottom: 10px; width: 90%;cursor: pointer;">
           <img src=${post.authorImage} style="height: 36px; width: 36px;" class="rounded-circle" />
           <div class="author-info d-flex" style="flex-direction: column; align-items: flex-start; margin-left: 15px;">  
-            <span class="avenirDemi author-name" style="font-size: 16px; line-height: 19px; color: rgba(0,0,0,0.8)">${postAuthor}</span>
+            <span class="avenirDemi author-name" style="font-size: 16px; line-height: 19px; color: rgba(0,0,0,0.8)">${author}</span>
             <span class="date text-muted" style="font-family:'AvenirNext LT Pro',sans-serif ;font-size: 12px; line-height: 24px; ">${post.published_on}</span>
           </div>
         </div>
