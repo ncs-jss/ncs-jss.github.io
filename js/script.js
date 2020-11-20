fe8410f3156f8be7654e47b6af2ce8f099016e5f
Rajat Verma <rajatverma5885045@gmail.com>
2020-11-20 17:26:52 +0530

    fix: preloader and cararosal conflict fixed4

--- js/script.js
+++ js/script.js
@@ -1,16 +1,15 @@
- var myVar;
+var myVar;
 
 function preloader() {
-    myVar = setTimeout(showPage, 6000);
+  myVar = setTimeout(showPage, 6000);
 }
 
 function showPage() {
-  document.getElementById("preloader").style.display = "none";
-  document.getElementById("content").style.transition = "all 0.9s ease";
-  document.getElementById("content").style.display = "block";
-  $("#panel_left").addClass("panel_left");
-        $("#panel_right").addClass("panel_right");
+  document.getElementById('preloader').style.display = 'none';
+  document.getElementById('content').style.transition = 'all 0.9s ease';
+  document.getElementById('content').style.visibility = 'visible';
+  document.getElementById('content').style.position = 'relative';
+
+  $('#panel_left').addClass('panel_left');
+  $('#panel_right').addClass('panel_right');
 }
-
-
-
