30108b671d5d8eb4512fa113bada74b872d4165d
Shruti3004 <mail2shruti.ag@gmail.com>
2020-12-26 20:52:08 +0530

    fixed the blogs design

--- js/blog.js
+++ js/blog.js
@@ -18,7 +18,7 @@ const addTemplate = (post) => {
   html = `
   <a style="text-decoration: none; color: #000000" class="aveniDemi" href=${post.link} target="_blank">
     <div class="col-md-3 pt-1 text-center" style="padding-left: 0; padding-right: 0;">
-      <div class="club_card blog_card justify-content-center mx-auto" style="margin-bottom: 15px; padding-bottom: 10px;">
+      <div class="club_card blog_card justify-content-center mx-auto" style="margin-bottom: 15px; padding-bottom: 12px;">
         <div class="card-img img-responsive mx-auto" style='width: 90%; min-height: 140px;  background: url("${post.image}"); background-size: cover;' alt="Card image cap"/>
       </div>
       <div class="card-body" style="padding-bottom: 0px; padding-left: 13px;" align="left">
@@ -33,8 +33,8 @@ const addTemplate = (post) => {
         <div class="footer d-flex mx-auto" style="padding-bottom: 10px; width: 90%;cursor: pointer;">
           <img src=${post.authorImage} style="height: 36px; width: 36px;" class="rounded-circle" />
           <div class="author-info d-flex" style="flex-direction: column; align-items: flex-start; margin-left: 15px;">  
-            <span class="avenirDemi author-name" style="font-size: 16px; line-height: 19px; color: rgba(0,0,0,0.8)">${author}</span>
-            <span class="date text-muted" style="font-family:'AvenirNext LT Pro',sans-serif ;font-size: 12px; line-height: 24px; ">${post.published_on}</span>
+            <span class="avenirDemi author-name" style="font-size: 14px; line-height: 19.12px; color: rgba(0,0,0,0.8)">${author}</span>
+            <span class="date text-muted" style="font-family:'AvenirNext LT Pro',sans-serif ;font-size: 12px; line-height: 16.39px; ">${post.published_on}</span>
           </div>
         </div>
         </a>
