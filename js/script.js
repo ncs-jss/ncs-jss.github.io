f1f11043b7b7df75040548867a2429c990cd59b1
Rajat Verma <rajatverma5885045@gmail.com>
2020-12-28 14:54:58 +0530

    fix: carousel prev button

--- js/script.js
+++ js/script.js
@@ -16,22 +16,19 @@ function showPage() {
 // Event Section JS
 document.addEventListener('DOMContentLoaded', function () {
   var elems = document.querySelectorAll('.carousel_section_ncs');
-  var instances = M.carousel_section_ncs.init(elems, {
+  M.carousel_section_ncs.init(elems, {
     padding: 200,
   });
 });
 
 function moveNextCarousel() {
-  var elems = document.querySelector(
-    '.carousel_section_ncs.carousel_section_ncs-slider'
-  );
+  var elems = document.querySelector('#carousel_id_ncs');
   var moveRight = M.carousel_section_ncs.getInstance(elems);
   moveRight.next(1);
 }
+
 function movePrevCarousel() {
-  var elems = document.querySelector(
-    '.carousel_section_ncs.carousel_section_ncs-slider'
-  );
+  var elems = document.querySelector('#carousel_id_ncs');
   var moveLeft = M.carousel_section_ncs.getInstance(elems);
   moveLeft.prev(1);
 }
