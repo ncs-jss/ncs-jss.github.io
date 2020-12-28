c5c15d08254102a21dd0c9d74bcf43220b1ae984
Rajat Verma <rajatverma5885045@gmail.com>
2020-12-28 20:45:43 +0530

    fix: remove conflicts

--- js/blog.js
+++ js/blog.js
@@ -1,14 +1,14 @@
 //getting section for showing blog data
-const section = document.querySelector("#blog-text");
+const section = document.querySelector('#blog-text');
 let html = ``;
 const getPosts = async () => {
   try {
-    const response = await fetch("../js/Blog.json");
+    const response = await fetch('../js/Blog.json');
     const data = await response.json();
     data.slice(-4).forEach((post) => addTemplate(post));
   } catch (error) {
     console.log(error);
-    section.parentElement.parentElement.style.display = "none";
+    section.parentElement.parentElement.style.display = 'none';
   }
 };
 
@@ -18,11 +18,11 @@ const addTemplate = (post) => {
   html = `
   <a style="text-decoration: none; color: #000000" class="aveniDemi" href=${post.link} target="_blank">
     <div class="col-md-3 pt-1 text-center" style="padding-left: 0; padding-right: 0;">
-      <div class="club_card blog_card justify-content-center mx-auto" style="margin-bottom: 15px; padding-bottom: 10px;">
-        <div class="card-img img-responsive mx-auto" style='width: 90%; min-height: 140px;  background: url("${post.image}"); background-size: cover;' alt="Card image cap"/>
-      </div>
+       <div class="club_card blog_card justify-content-center mx-auto" style="margin-bottom: 15px; padding-bottom: 10px;">
+        <div class="card-img img-responsive mx-auto" style='width: 90%;' alt="Card image cap"/><img src=${post.image} class="img-fluid rounded" style='height:140px'/>
+      </div>      
       <div class="card-body" style="padding-bottom: 0px; padding-left: 13px;" align="left">
-        <h4 title="${title}" class="avenirDemi" style="font-weight: 500; font-size: 24px; line-height: 33px;">${title}</h4>
+        <h4 title="${title}" class="avenirDemi blog_title" style="font-weight: 500; font-size: 24px; line-height: 33px;">${title}</h4>
         <a style="color: #000000" href="${post.link}" target="_blank">
         <div class="source" style="margin-top: 7px;">
           <img style="height: 20px; width: 20px" src="../img/icons/blog/${post.source}.svg"</img></div>
@@ -33,8 +33,8 @@ const addTemplate = (post) => {
         <div class="footer d-flex mx-auto" style="padding-bottom: 10px; width: 90%;cursor: pointer;">
           <img src=${post.authorImage} style="height: 36px; width: 36px;" class="rounded-circle" />
           <div class="author-info d-flex" style="flex-direction: column; align-items: flex-start; margin-left: 15px;">  
-            <span class="avenirDemi author-name" style="font-size: 16px; line-height: 19px; color: rgba(0,0,0,0.8)">${author}</span>
-            <span class="date text-muted" style="font-family:'AvenirNext LT Pro',sans-serif ;font-size: 12px; line-height: 24px; ">${post.published_on}</span>
+            <span class="avenirDemi author-name" style="font-size: 14px; line-height: 19.12px; color: rgba(0,0,0,0.8)">${author}</span>
+            <span class="date text-muted" style="font-family:'AvenirNext LT Pro',sans-serif ;font-size: 12px; line-height: 16.39px; ">${post.published_on}</span> 
           </div>
         </div>
         </a>
